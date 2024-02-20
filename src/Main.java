75dccefd4c400cccd848ac0ad070c9b822fe8ff9
Fernando Peralta Castro <peraltaf@moravian.edu>
2024-02-20 17:49:57 -0500

    Removed additional files from src folder, and added Main.java and Roster.java files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package cs234_Project;
+
+import javax.swing.*;
+
+public class Main {
+
+    /**
+     * The main method to launch the application.
+     * @param args Command-line arguments (not used).
+     */
+    public static void Main(String[] args) {
+
+        Roster roster = new Roster();
+    }
+}
